13122f3ff0a78f65f329babb0d050c0659bc8862
Ubuntu <adedolapo@Adedolapo-VM.qwpbnjkvoqqejbfbw2rcyrf1sf.parx.internal.cloudapp.net>
2026-04-17 19:07:58 +0000

    Created chapter_01 and chapter_02 folde, documenting my learning progress

---
+++ chapter_02/variables.py
@@ -0,0 +1,8 @@
+greeting_message = "Hello, Python World!"
+f_name = "Adedolapo"
+l_name = " Adeboye "
+age = 23 + " years old"
+occupation = "A student"
+address = "Room 4, block A, Sultan Bello Hall, University of Ibadan, Ibadan, Oyo State, Nigeria."
+full_name = f"{f_name} {l_name.rstrip().lstrip()}"
+print(greeting_message + " I'm " + full_name + ", a" + age + "\n" occupation)
